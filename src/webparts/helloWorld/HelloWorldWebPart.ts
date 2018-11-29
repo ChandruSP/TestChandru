63fbe05b7e7eb179430030d08948fc6b6b8d3845
ChandruSP <dchandramoorthy@gmail.com>
2018-11-29 14:48:05 +0530

    Update HelloWorldWebPart.ts

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -16,7 +16,7 @@ export interface IHelloWorldWebPartProps {
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
 
   public render(): void {
-    this.domElement.innerHTML = "New file";
+    this.domElement.innerHTML = "All are changed";
   }
 
   protected get dataVersion(): Version {
