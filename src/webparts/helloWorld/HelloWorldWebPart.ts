f6210580f8fa5b4f5f4e81ef9756c5cc95fc2321
kameshm05 <45416518+kameshm05@users.noreply.github.com>
2018-11-29 15:16:17 +0530

    sderet
    change change

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -16,7 +16,7 @@ export interface IHelloWorldWebPartProps {
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
 
   public render(): void {
-    this.domElement.innerHTML = "All are changed";
+    this.domElement.innerHTML = "All are change";
   }
 
   protected get dataVersion(): Version {
